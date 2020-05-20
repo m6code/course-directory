0e211bfac666f731f16efee44df5f515cc2f6d7e
M6code <benjamynfrank101@gmail.com>
2020-05-20 13:26:20 +0100

    Updates

---
+++ precache-manifest.1b5b805fb4f92fb1798fd115772719e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09d47bbb2d1e710a4616bc008aecfaf",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "afd08ad13b2fd0d59eb7",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "d8df68c52cf91be55f3c",
+    "url": "/course-directory/static/js/2.5323fd21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/course-directory/static/js/2.5323fd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd08ad13b2fd0d59eb7",
+    "url": "/course-directory/static/js/main.e043262a.chunk.js"
+  },
+  {
+    "revision": "2a7e4e8b47cdf83ce1f6",
+    "url": "/course-directory/static/js/runtime-main.d75459dc.js"
+  }
+]);
